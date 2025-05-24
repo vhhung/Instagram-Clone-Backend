d691845268d630f606f7fc3e2aa66905f6f02516
Hung Nguyen <vh.hungn@gmail.com>
2025-05-24 16:11:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ from app import create_app
 #   return f"Hello {name}!"
 
 app = create_app()
+
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
